c331ac677f251c9a192a08673d6d80e5594c4aa5
Jan Spencer Catalan <jancatalan2@gmail.com>
2018-10-03 02:10:52 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,6 @@
 function forLoop(){
   for(let i = 0; i<26;i++){
-    if(i==1){
-      return "I am 1 strange loop.";
-    }
-    else{
-      return "I am " + i + " strange loops." 
-    }
+    if(i==1)  return "I am 1 strange loop.";
+    if(i!=1) return "I am " + i + " strange loops." ;
   }
 }
